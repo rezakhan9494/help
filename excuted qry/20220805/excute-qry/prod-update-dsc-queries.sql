bf36cba6b6f64276a3104bdb179aa08d36092a1b
Md Reza Khan <rezakhan9494@gmail.com>
2022-08-05 10:02:58 +0000

    update prod issue qry

---
+++ excuted qry/20220805/excute-qry/prod-update-dsc-queries.sql
@@ -0,0 +1,8 @@
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-RRK-2022-02-22-001059');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-BDA-2022-05-02-003118');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-RRK-2022-06-01-003983');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-RRK-2021-11-12-000150');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-BMC-2022-06-02-004042');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-BMC-2022-03-20-001838');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-RRK-2022-04-06-002272');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-BMC-2022-03-21-001865');
