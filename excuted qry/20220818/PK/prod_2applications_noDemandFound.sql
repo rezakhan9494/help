1515334c4b98d13400e87b711a0929e23b8bc353
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-08-18 15:22:31 +0530

    no demands found: send back to approval inprogress

---
+++ excuted qry/20220818/PK/prod_2applications_noDemandFound.sql
@@ -0,0 +1,16 @@
+--issue:no demand found error for 2 applications while loading payment page for permit fee--
+--BP-BAM-2022-06-29-005539,BP-BAM-2022-06-29-005539--
+--observations:No installments,dsc table entry present(need to delete)--
+
+--Need to check if demands exist--
+select * from egbs_demand_v1 where consumercode='BP-BAM-2022-06-29-005539' and  businessservice='BPA.NC_SAN_FEE';
+select * from egbs_demand_v1 where consumercode='BP-BAM-2022-06-22-004994' and  businessservice='BPA.NC_SAN_FEE';
+
+--If demands also not exists, then We need to send back one step these two applications--
+delete from eg_bpa_dscdetails where applicationno='BP-BAM-2022-06-29-005539';
+update eg_bpa_buildingplan set status='APPROVAL_INPROGRESS' where applicationno='BP-BAM-2022-06-29-005539';
+delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 ewpv where businessid='BP-BAM-2022-06-29-005539' and action='APPROVE' order by createdtime desc limit 1);
+
+delete from eg_bpa_dscdetails where applicationno='BP-BAM-2022-06-22-004994';
+update eg_bpa_buildingplan set status='APPROVAL_INPROGRESS' where applicationno='BP-BAM-2022-06-22-004994';
+delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 ewpv where businessid='BP-BAM-2022-06-22-004994' and action='APPROVE' order by createdtime desc limit 1);
