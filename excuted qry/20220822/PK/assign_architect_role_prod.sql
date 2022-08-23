0e91bc09b8e843a908a21c81e522704da160ff26
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-08-23 10:42:46 +0530

    assign architect role directly

---
+++ excuted qry/20220822/PK/assign_architect_role_prod.sql
@@ -0,0 +1,6 @@
+INSERT INTO eg_userrole_v1(
+	role_code, role_tenantid, user_id, user_tenantid, lastmodifieddate)
+	VALUES ('BPA_ARCHITECT', 'od', 3056517, 'od', '2022-08-23 16:12:59.246');
+INSERT INTO eg_userrole_v1(
+	role_code, role_tenantid, user_id, user_tenantid, lastmodifieddate)
+	VALUES ('BPA_ARCHITECT', 'od', 2869378, 'od', '2022-08-23 16:12:59.246');
