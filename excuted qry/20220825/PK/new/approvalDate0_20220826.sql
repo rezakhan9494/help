e08ce376abd6547cd20a650204dfa223b29b9abd
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-08-26 12:27:22 +0530

    correct approvaldate for 3 applications and delink their old permit letters

---
+++ excuted qry/20220825/PK/new/approvalDate0_20220826.sql
@@ -0,0 +1,12 @@
+--set approval date from date of payment--
+--BP-CTC-2022-07-23-006490
+--BP-CTC-2022-08-03-007081
+--BP-CTC-2022-06-23-005066
+update eg_bpa_buildingplan set approvaldate = (select createdtime from eg_wf_processinstance_v2 ewpv where businessid='BP-CTC-2022-07-23-006490' and action='PAY' order by createdtime desc limit 1) where applicationno ='BP-CTC-2022-07-23-006490';
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-CTC-2022-07-23-006490');
+
+update eg_bpa_buildingplan set approvaldate = (select createdtime from eg_wf_processinstance_v2 ewpv where businessid='BP-CTC-2022-08-03-007081' and action='PAY' order by createdtime desc limit 1) where applicationno ='BP-CTC-2022-08-03-007081';
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-CTC-2022-08-03-007081');
+
+update eg_bpa_buildingplan set approvaldate = (select createdtime from eg_wf_processinstance_v2 ewpv where businessid='BP-CTC-2022-06-23-005066' and action='PAY' order by createdtime desc limit 1) where applicationno ='BP-CTC-2022-06-23-005066';
+update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-CTC-2022-06-23-005066');
