f5347ae0f2d6077ee681c57b49ee9020eae9d7ed
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-08-26 15:55:51 +0530

    issues added

--- excuted qry/20220825/PK/new/approvalDate0_20220826.sql
+++ excuted qry/20220825/PK/new/approvalDate0_20220826.sql
@@ -3,10 +3,10 @@
 --BP-CTC-2022-08-03-007081
 --BP-CTC-2022-06-23-005066
 update eg_bpa_buildingplan set approvaldate = (select createdtime from eg_wf_processinstance_v2 ewpv where businessid='BP-CTC-2022-07-23-006490' and action='PAY' order by createdtime desc limit 1) where applicationno ='BP-CTC-2022-07-23-006490';
-update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-CTC-2022-07-23-006490');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where applicationno='BP-CTC-2022-07-23-006490';
 
 update eg_bpa_buildingplan set approvaldate = (select createdtime from eg_wf_processinstance_v2 ewpv where businessid='BP-CTC-2022-08-03-007081' and action='PAY' order by createdtime desc limit 1) where applicationno ='BP-CTC-2022-08-03-007081';
-update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-CTC-2022-08-03-007081');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where applicationno='BP-CTC-2022-08-03-007081';
 
 update eg_bpa_buildingplan set approvaldate = (select createdtime from eg_wf_processinstance_v2 ewpv where businessid='BP-CTC-2022-06-23-005066' and action='PAY' order by createdtime desc limit 1) where applicationno ='BP-CTC-2022-06-23-005066';
-update eg_bpa_dscdetails set documenttype=null,documentid=null where buildingplanid = (select id from eg_bpa_buildingplan ebb where applicationno='BP-CTC-2022-06-23-005066');
+update eg_bpa_dscdetails set documenttype=null,documentid=null where applicationno='BP-CTC-2022-06-23-005066';
---
+++ excuted qry/20220825/PK/new/approver_change_BP-BDA-2022-07-12-006013.sql
@@ -0,0 +1,2 @@
+--1.dsc approver update BP-BDA-2022-07-12-006013--
+update eg_bpa_dscdetails set documenttype=null,documentid=null,approvedby='327a7d40-50d5-4dc4-be6f-de06b4e8831c' where applicationno='BP-BDA-2022-07-12-006013';
