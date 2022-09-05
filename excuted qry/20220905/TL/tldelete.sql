2254104029ddc8f7a84ae7e26a6b5e4df0c1e1ed
Md Reza Khan <60547570+rezakhan9494@users.noreply.github.com>
2022-09-05 17:50:11 +0530

    Add files via upload

---
+++ excuted qry/20220905/TL/tldelete.sql
@@ -0,0 +1,9 @@
+update eg_tl_billingslab  set tenantid ='od.anandpur_delete' where tradetype ='NONEATINGNONMEDICALESTABLISHMENTS.TST227' and tenantid='od.anandapur';
+
+update eg_tl_billingslab  set tenantid ='od.nimapara_delete' where tradetype ='NONEATINGNONMEDICALESTABLISHMENTS.TST292' and tenantid='od.nimapara';
+
+update eg_tl_billingslab  set tenantid ='od.nimapara_delete' where tradetype ='NONEATINGNONMEDICALESTABLISHMENTS.TST155' and tenantid='od.nimapara';
+
+update eg_tl_billingslab  set tenantid ='od.digapahandi_delete' where tradetype ='EATINGESTABLISHMENTS.TST33' and tenantid='od.digapahandi';
+
+update eg_tl_billingslab  set tenantid ='od.rourkela_delete' where tradetype ='EATINGESTABLISHMENTS.TST3' and tenantid='od.rourkela';
