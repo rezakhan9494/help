07584a093615a64b1b298cd76a38522eca339fc3
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-09-05 11:15:46 +0530

    check user id

---
+++ excuted qry/20220822/PK/check_user_id.sql
@@ -0,0 +1,4 @@
+select id from eg_user where uuid=
+(select createdby from eg_tl_tradelicense ett where ett.applicationnumber='BPR-2022-01-10-001068');
+select id from eg_user where uuid=
+(select createdby from eg_tl_tradelicense ett where ett.applicationnumber='BPR-2022-01-20-001363');
