0d8b94c83201c79d67b79445a3c49534affb69dc
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-09-06 18:09:06 +0530

    duplicate documents deletion

---
+++ excuted qry/20220906/PK/BP-BMC-2022-05-04-003189/BP-BMC-2022-05-04-003189_delete_duplicate.sql
@@ -0,0 +1,12 @@
+--------duplicate docs deletion for BP-BMC-2022-05-04-003189
+delete from eg_bpa_document where id in 
+('8b93804e-c038-41e0-90f2-2d8cc325c0fb',
+'09b4e4d8-5af9-4e6d-a65f-f3bf7556d899',
+'41510410-da65-4688-9044-8237b71cfeea',
+'6a1e266c-0cd0-4fcf-81a1-8473c9ac19c0',
+'a8b8777b-b502-4a1e-a844-6d8efa5f0bd4',
+'a45dba09-1985-4a10-afb9-3f94709a3ac4',
+'ba7a3548-538a-4974-98b9-1e87163c2936',
+'31ffb759-fc50-4f6e-8e44-1ca0b7d7459a',
+'e83bb5b7-3379-452c-b38d-99c856fdbee4',
+'854b78a4-a12e-4b35-9168-b6a90b3768fd');
