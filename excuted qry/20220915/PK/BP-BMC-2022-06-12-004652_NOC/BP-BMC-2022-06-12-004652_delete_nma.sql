c77973afc4f9ded143e3df393cfa31a2db31576b
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-09-15 17:42:46 +0530

    noc issue

---
+++ excuted qry/20220915/PK/BP-BMC-2022-06-12-004652_NOC/BP-BMC-2022-06-12-004652_delete_nma.sql
@@ -0,0 +1,4 @@
+--BP-BMC-2022-06-12-004652 delete NOC
+delete from eg_noc_document end2 where nocid in
+(select id from eg_noc where sourcerefid ='BP-BMC-2022-06-12-004652' and noctype='NMA_NOC');
+delete from eg_noc where sourcerefid ='BP-BMC-2022-06-12-004652' and noctype='NMA_NOC';
