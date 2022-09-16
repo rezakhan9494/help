3edd38dcd15c63e10c0d2962269342a7d7438d83
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-09-16 17:55:55 +0530

    duplicate docs

---
+++ excuted qry/20220916/duplicate_docs.sql
@@ -0,0 +1,10 @@
+--------duplicate docs deletion for BP-BDA-2022-04-20-002668
+delete from eg_bpa_document where id in 
+('5348b62d-9f8f-4cae-b014-0a2c06d3afa4',
+'efe3d570-fff1-417c-9935-fb0178123044');
+
+
+--------duplicate docs deletion for BP-BDA-2022-08-08-007297
+delete from eg_bpa_document where id in 
+('7b31e0df-777f-4c7a-8a0e-8260e96310a7',
+'81a85915-a8e4-425c-a7cf-4444b253ab31');
