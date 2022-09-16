fc4bc2d963fef34035e1d9867824f74c793d9c35
prasunTranserve <80515762+prasunTranserve@users.noreply.github.com>
2022-09-16 11:24:58 +0530

    rework

---
+++ excuted qry/20220915/PK/rework/BP-BMC-2022-03-15-001707_rework_send_oneStepBack.sql
@@ -0,0 +1,4 @@
+--send back one step back BP-BMC-2022-03-15-001707--
+delete from eg_wf_processinstance_v2 where id=(select id from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv where ewpv.businessid='BP-BMC-2022-03-15-001707');
+
+update eg_bpa_buildingplan set status= (select applicationStatus from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv inner join eg_wf_state_v2 ewsv on ewsv.uuid=ewpv.status where ewpv.businessid ='BP-BMC-2022-03-15-001707') where applicationno ='BP-BMC-2022-03-15-001707';
