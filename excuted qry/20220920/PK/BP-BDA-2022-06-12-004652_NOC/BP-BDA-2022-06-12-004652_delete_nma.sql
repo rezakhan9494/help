44ce7fb40f8ba1699e385862bf9bb851c9bf77a6
Md Reza Khan <60547570+rezakhan9494@users.noreply.github.com>
2022-09-22 11:13:31 +0530

    Update BP-BDA-2022-06-12-004652_delete_nma.sql

--- excuted qry/20220920/PK/BP-BDA-2022-06-12-004652_NOC/BP-BDA-2022-06-12-004652_delete_nma.sql
+++ excuted qry/20220920/PK/BP-BDA-2022-06-12-004652_NOC/BP-BDA-2022-06-12-004652_delete_nma.sql
@@ -2,3 +2,4 @@
 delete from eg_noc_document end2 where nocid in
 (select id from eg_noc where sourcerefid ='BP-BDA-2022-06-12-004652' and noctype='NMA_NOC');
 delete from eg_noc where sourcerefid ='BP-BDA-2022-06-12-004652' and noctype='NMA_NOC';
+--done
