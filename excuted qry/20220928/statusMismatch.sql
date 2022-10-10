0c2c55f07ea3752cb6a94a977aa9c89f56f772c3
Ankit <ankit.k.kumar.singh@pwc.com>
2022-10-10 10:52:14 +0530

    query push

--- excuted qry/20220928/statusMismatch.sql
+++ excuted qry/20220928/statusMismatch.sql
@@ -8,9 +8,12 @@ update eg_bpa_buildingplan set status=(select applicationStatus from (select dis
 
 delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 where businessid='BP-BDA-2022-08-08-007269' order by createdtime desc limit 1);
 
-update eg_bpa_buildingplan set status=(select applicationStatus from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv inner join eg_wf_state_v2 ewsv on ewsv.uuid=ewpv.status where ewpv.businessid ='BP-BDA-2022-02-18-000955') where applicationno ='BP-BDA-2022-08-08-007269';
+update eg_bpa_buildingplan set status=(select applicationStatus from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv inner join eg_wf_state_v2 ewsv on ewsv.uuid=ewpv.status where ewpv.businessid ='BP-BDA-2022-08-08-007269') where applicationno ='BP-BDA-2022-08-08-007269';
 
 
+
+
+#mismatch at Approver installment also generated
 
 delete from eg_bpa_installment where consumercode in ('BP-BAM-2022-06-13-004654');
 
