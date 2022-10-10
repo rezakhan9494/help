a15dacdcf4a332a2c21a25959f74ac6fc856b44e
Ankit <ankit.k.kumar.singh@pwc.com>
2022-10-10 11:23:30 +0530

    jatni application query removed

--- excuted qry/20220928/statusMismatch.sql
+++ excuted qry/20220928/statusMismatch.sql
@@ -13,34 +13,7 @@ update eg_bpa_buildingplan set status=(select applicationStatus from (select dis
 
 
 
-#mismatch at Approver installment also generated
-
-delete from eg_bpa_installment where consumercode in ('BP-BAM-2022-06-13-004654');
-
-
-delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 where businessid='BP-BAM-2022-06-13-004654' order by createdtime desc limit 1);
-
-
-
-
-update eg_bpa_buildingplan set status=(select applicationStatus from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv inner join eg_wf_state_v2 ewsv on ewsv.uuid=ewpv.status where ewpv.businessid ='BP-BAM-2022-06-13-004654') where applicationno ='BP-BAM-2022-06-13-004654';
-
-
-
-
-
-#no installment generated  for this
-
-delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 where businessid='BP-JTN-2022-08-03-007071' order by createdtime desc limit 1);
-
-
-
-
-update eg_bpa_buildingplan set status=(select applicationStatus from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv inner join eg_wf_state_v2 ewsv on ewsv.uuid=ewpv.status where ewpv.businessid ='BP-JTN-2022-08-03-007071') where applicationno ='BP-JTN-2022-08-03-007071';
-
-
-
-#no installment generated  for this
+#no installment generated  for this status mismatch at approval 
 
 
 delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 where businessid='BP-RRK-2022-08-03-007048' order by createdtime desc limit 1);
