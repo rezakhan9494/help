67c7fb4872726f5b969c55d07320b05725347724
prasunTranserve <prasun.kumar@transerve.com>
2022-10-19 18:25:39 +0530

    counts of query results

--- excuted qry/20221019/PK/multiple_demands_applications/BP-RRK-2022-07-26-006558/BP-RRK-2022-07-26-006558_analysis.sql
+++ excuted qry/20221019/PK/multiple_demands_applications/BP-RRK-2022-07-26-006558/BP-RRK-2022-07-26-006558_analysis.sql
@@ -1,24 +1,24 @@
 --BP-RRK-2022-07-26-006558
 select * from egbs_demand_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
-'BPA.NC_SAN_FEE';
+'BPA.NC_SAN_FEE';--count=13
 select * from egbs_demanddetail_v1 where demandid in 
 (select id from egbs_demand_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
-'BPA.NC_SAN_FEE');
+'BPA.NC_SAN_FEE');--count=104
 select * from egbs_billdetail_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
-'BPA.NC_SAN_FEE';
+'BPA.NC_SAN_FEE';--count=91
 select * from egbs_billaccountdetail_v1 where billdetail in 
 (select id from egbs_billdetail_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
-'BPA.NC_SAN_FEE');
+'BPA.NC_SAN_FEE');--count=700
 select * from egbs_bill_v1 where id in 
 (select billid from egbs_billdetail_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
-'BPA.NC_SAN_FEE');
+'BPA.NC_SAN_FEE');--count=13
 
 
 --collection bill tables--
-select * from egcl_bill where consumercode='BP-RRK-2022-07-26-006558' and businessservice ='BPA.NC_SAN_FEE';
+select * from egcl_bill where consumercode='BP-RRK-2022-07-26-006558' and businessservice ='BPA.NC_SAN_FEE';--count=0
 select * from egcl_billdetial eb2 where billid in
-(select id from egcl_bill where consumercode='BP-RRK-2022-07-26-006558' and businessservice ='BPA.NC_SAN_FEE');
+(select id from egcl_bill where consumercode='BP-RRK-2022-07-26-006558' and businessservice ='BPA.NC_SAN_FEE');--count=0
 select * from egcl_billaccountdetail eb 
 where billdetailid in
 (select id from egcl_billdetial eb2 where billid in
-(select id from egcl_bill where consumercode='BP-RRK-2022-07-26-006558' and businessservice ='BPA.NC_SAN_FEE'));
+(select id from egcl_bill where consumercode='BP-RRK-2022-07-26-006558' and businessservice ='BPA.NC_SAN_FEE'));--count=0
