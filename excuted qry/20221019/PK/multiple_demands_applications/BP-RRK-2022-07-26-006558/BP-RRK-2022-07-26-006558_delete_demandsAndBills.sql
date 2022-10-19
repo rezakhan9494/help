eba0c86f378ecbbc80aa1b798ba54647211675e6
prasunTranserve <prasun.kumar@transerve.com>
2022-10-19 18:32:31 +0530

    delete demands and bill queries for multiple demands issue

---
+++ excuted qry/20221019/PK/multiple_demands_applications/BP-RRK-2022-07-26-006558/BP-RRK-2022-07-26-006558_delete_demandsAndBills.sql
@@ -0,0 +1,16 @@
+--BP-RRK-2022-07-26-006558
+delete from egbs_demand_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
+'BPA.NC_SAN_FEE';
+delete from egbs_demanddetail_v1 where demandid in 
+(select id from egbs_demand_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
+'BPA.NC_SAN_FEE');
+delete from egbs_billdetail_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
+'BPA.NC_SAN_FEE';
+delete from egbs_billaccountdetail_v1 where billdetail in 
+(select id from egbs_billdetail_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
+'BPA.NC_SAN_FEE');
+delete from egbs_bill_v1 where id in 
+(select billid from egbs_billdetail_v1 where consumercode='BP-RRK-2022-07-26-006558' and businessservice=
+'BPA.NC_SAN_FEE');
+
+update eg_bpa_installment set demandId=null where consumercode='BP-RRK-2022-07-26-006558';
