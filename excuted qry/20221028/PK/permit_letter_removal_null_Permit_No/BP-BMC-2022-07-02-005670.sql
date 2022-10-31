eef2405abe9e2ea453ef5a3c2c91651188aa3e6b
Md Reza Khan <60547570+rezakhan9494@users.noreply.github.com>
2022-10-31 19:51:34 +0530

    Update BP-BMC-2022-07-02-005670.sql

--- excuted qry/20221028/PK/permit_letter_removal_null_Permit_No/BP-BMC-2022-07-02-005670.sql
+++ excuted qry/20221028/PK/permit_letter_removal_null_Permit_No/BP-BMC-2022-07-02-005670.sql
@@ -1,2 +1,5 @@
 --BP-BMC-2022-07-02-005670 remove old permit letter--
 update eg_bpa_dscdetails set documenttype=null,documentid=null where applicationno='BP-BMC-2022-07-02-005670';
+
+
+filestore/od.bhubaneswar/BPA/October/31/1667225948383buildingpermit_signed.pdf
