e1c9ca969c275ac3fcb20c657faa894fd9d8d999
prasunTranserve <prasun.kumar@transerve.com>
2022-11-26 15:45:49 +0530

    adding BPA4 to backup queries

--- excuted qry/20221124/workflow_backups.sql
+++ excuted qry/20221124/workflow_backups.sql
@@ -1,6 +1,6 @@
-select * from eg_wf_businessservice_v2 ewbv where businessservice in ('BPA1','BPA2','BPA3','BPA6');
+select * from eg_wf_businessservice_v2 ewbv where businessservice in ('BPA1','BPA2','BPA3','BPA4','BPA6');
 select * from eg_wf_state_v2 ewsv where businessserviceid in 
-(select uuid from eg_wf_businessservice_v2 ewbv where businessservice in ('BPA1','BPA2','BPA3','BPA6'));
+(select uuid from eg_wf_businessservice_v2 ewbv where businessservice in ('BPA1','BPA2','BPA3','BPA4','BPA6'));
 select * from eg_wf_action_v2 ewav where currentstate in
 (select uuid from eg_wf_state_v2 ewsv where businessserviceid in 
-(select uuid from eg_wf_businessservice_v2 ewbv where businessservice in ('BPA1','BPA2','BPA3','BPA6')));
+(select uuid from eg_wf_businessservice_v2 ewbv where businessservice in ('BPA1','BPA2','BPA3','BPA4','BPA6')));
