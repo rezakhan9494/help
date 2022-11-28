d1b40f988cad7ed7131bb1e6e7b412916ad5b6a7
Ankit <ankit.k.kumar.singh@pwc.com>
2022-11-28 10:30:15 +0530

    STATUS MISMATCH

--- excuted qry/20221128/statusmismatch.sql
+++ excuted qry/20221128/statusmismatch.sql
@@ -68,18 +68,5 @@ update eg_bpa_buildingplan set status=(select applicationStatus from (select dis
 
 
 
--- status mismatch at Approver Level
 
-delete from  public.eg_bpa_installment where consumercode  in ('BP-SNG-2022-08-24-007750');
-
-delete from  eg_bpa_dscdetails where applicationno ='BP-SNG-2022-08-24-007750' ;
-
-
-
-delete from eg_wf_processinstance_v2 where id=(select id from eg_wf_processinstance_v2 where businessid='BP-SNG-2022-08-24-007750' order by createdtime desc limit 1);
-
-
-
-
-update eg_bpa_buildingplan set status=(select applicationStatus from (select distinct on(businessid) * from eg_wf_processinstance_v2 ewpv order by businessid,createdtime desc) ewpv inner join eg_wf_state_v2 ewsv on ewsv.uuid=ewpv.status where ewpv.businessid ='BP-SNG-2022-08-24-007750') where applicationno ='BP-SNG-2022-08-24-007750';
 
