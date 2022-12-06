4e82b2a4da11f91e7a5a49f05680fd62e39bc1d4
prasunTranserve <prasun.kumar@transerve.com>
2022-12-06 11:41:45 +0530

    correction in query

--- excuted qry/20221201/BP-BMC-2022-11-22-010501/BP-BMC-2022-11-22-010501_processinstance_update.sql
+++ excuted qry/20221201/BP-BMC-2022-11-22-010501/BP-BMC-2022-11-22-010501_processinstance_update.sql
@@ -1,3 +1,3 @@
 --BP-BMC-2022-11-22-010501 set correct user uuid in processinstance as one character extra--
 update eg_wf_processinstance_v2 set createdby='255c3fcf-a811-427a-8ec5-cdb88a1c709b'
-,lastmodifiedby='255c3fcf-a811-427a-8ec5-cdb88a1c709b' where action='PAY';
+,lastmodifiedby='255c3fcf-a811-427a-8ec5-cdb88a1c709b' where action='PAY' and businessid='BP-BMC-2022-11-22-010501';
